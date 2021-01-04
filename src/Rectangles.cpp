6a38e1564f8ab0fe8f248f64bba7d302d95b6d0a
bensandison <sandisonben@gmail.com>
2021-01-04 15:57:20 +0000

    created some rectangles

--- src/Rectangles.cpp
+++ src/Rectangles.cpp
@@ -9,5 +9,9 @@ void Rectangles::update(){
 }
 
 void Rectangles::draw(){
-    
+    ofFill();
+    ofSetColor(200, 0, 0);
+    ofDrawRectangle(200, 200, 100, 100);
+    ofSetColor(0, 200, 20);
+    ofDrawRectangle(500, 500, 150, 200);
 }
